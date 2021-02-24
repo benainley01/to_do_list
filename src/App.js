aaa30fa8cc48ba4712eea694dd9a7361d05208cd
benainley01 <60328325+benainley01@users.noreply.github.com>
2021-02-24 14:47:28 -0500

    uploading project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import AddBar from './Components/AddBar.js'
 import './App.css';
+import { message } from 'antd';
+import "antd/dist/antd.css";
+import TaskRows from './Components/TaskRows.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      items: []
+    }
+  }
+
+  addTask = task => {
+    if (task == "") {
+      message.error("Task has to contain something")
+    } else {
+      let prevState = this.state.items
+      prevState.push(task);
+      this.setState({
+        items: prevState
+      });
+    }
+  }
+
+  // removeTask = key => {
+  //   let prevState = this.state.items.filter(item => !(item == key));
+  //   this.setState({
+  //     items: prevState
+  //   });
+  // }
+  
+  removeTask = key => {
+    let prevState = []
+    this.state.items.forEach(item=>{
+      if(item!=key){
+        prevState.push(item)
+      }
+    })
+    this.setState({
+      items: prevState
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="whole">
+        <h1>
+          Tasks
+        </h1>
+        <hr></hr>
+        <div>
+          <TaskRows
+            items={this.state.items}
+            removeTask={this.removeTask} />
+        </div>
+        <hr></hr>
+        <div>
+          <AddBar
+            addTask={this.addTask} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
